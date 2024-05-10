d5f120685b2fd97888f057a51411c9681fc13388
Matheus Padilha Ansolin <56612773+MatheusAnsolin@users.noreply.github.com>
2024-05-10 10:05:34 -0300

    Add files via upload

--- Spotifaula/Spotifaula/MusicDetail.swift
+++ Spotifaula/Spotifaula/MusicDetail.swift
@@ -9,19 +9,32 @@ import SwiftUI
 
 struct MusicDetail: View {
 
-    var music : Song
+   @State var music: Song
     
     var body: some View {
         NavigationStack{
-            
-            Image("\(music.cover)")
-            Text("\(music.name)")
-            Text("\(music.artist)")
-            
+            Spacer()
+            AsyncImage(url: URL(string: (music.cover))){ image in
+                    image.resizable().aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    Image(systemName: "photo.fill")
+                }.frame(width: 200, height: 200)
+            Text(music.name)
+            Text(music.artist)
+            Spacer()
+            HStack(){
+                Image(systemName: "shuffle").frame(height: 45)
+                Image(systemName: "backward.end.fill").frame(width:45)
+                Image(systemName: "play.fill").frame(width: 45)
+                Image(systemName: "forward.end.fill").frame(width: 45)
+                Image(systemName: "repeat").frame(height: 45)
+            }
+            Spacer()
         }
     }
 }
 
+//dados mockados para preview e estilização
 #Preview {
-    MusicDetail()
+    MusicDetail(music: Song(id: 6, name: "Outro Nome", artist: "Artista Mesmo", cover: "https://source.unsplash.com/random/200x200"))
 }
